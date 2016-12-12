d00672cbeceb6d7b6a2c751799a7260854a1a29c
Cody Brubaker and Jim Padilla <cody@>
2016-12-12 13:22:02 -0800

    add passing spec for project attribute in employee

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -3,10 +3,18 @@ require('spec_helper')
 describe(Employee) do
 
   describe('#division') do
-    it ('Tells which division it belongs to') do
+    it ('Tells which division the employee belongs to') do
       test_division = Division.create({:name => 'Money'})
       test_employee = Employee.create({:name => 'Cody', :division_id => test_division.id})
       expect(test_employee.division()).to(eq(test_division))
+    end
+  end
+
+  describe('#project') do
+    it ('Tells which project the employee belongs to') do
+      test_project = Project.create({:description => 'Count money'})
+      test_employee = Employee.create({:name => 'Cody', :project_id => test_project.id})
+      expect(test_employee.project()).to(eq(test_project))
     end
   end
 
